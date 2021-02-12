4da6816c1b4db0aa30396b6805734a7e8cae3de5
bakhshir <rena@bakhshi.net>
2021-02-12 23:10:25 +0100

    create initials img

--- scripts/text2img.py
+++ scripts/text2img.py
@@ -4,17 +4,18 @@ from PIL import Image, ImageDraw, ImageFont
 import os
 
 
-def text_on_img(filename='../data/avatars/01.png', text="Hello", size=10, color=(255, 255, 0), bg='white'):
+def text_on_img(filename='01.png', text="Hello", size=10, color=(0, 0, 0), bg='white'):
     "Draw a text on an Image, saves it, show it"
-    font = ImageFont.truetype('../arial.ttf', size)
+    font = ImageFont.truetype('../data/players/arial.ttf', size)
     # create image
     image = Image.new(mode="RGB", size = (3*int(size / 4) * len(text), size + 50), color=bg)
     draw = ImageDraw.Draw(image)
     # draw text
-    draw.text((10, 10), text, font=font, fill=(0, 0, 0))
+    draw.text((10, 10), text, font=font, fill=color)
     # save file
     image.save(filename)
 
 input_txt = "RB"
-fname = '../data/avatars/'+input_txt.lower()+'.png'
+players_path = '../data/players/'
+fname = players_path + 'avatars/'+input_txt.lower()+'.png'
 text_on_img(filename=fname,text=input_txt, size=300, bg='white')
